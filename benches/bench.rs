27d7284173a3dfcf3b81b1606043d05de832323b
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-03 22:52:29 +0000

    Add benches
    Prove that ndc logger does not create a significant overhead
    
    ```
    ...
    running 3 tests
    test log               ... bench:           0 ns/iter (+/- 0)
    test log_ndc_empty     ... bench:           0 ns/iter (+/- 0)
    test log_ndc_not_empty ... bench:           0 ns/iter (+/- 0)
    ```

---
+++ benches/bench.rs
@@ -0,0 +1,48 @@
+// `cargo test --benches` and `#[feature(test)]` work only in nightly
+#![cfg(rustc_nightly)]
+#![feature(test)]
+
+extern crate test;
+
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate log_ndc;
+
+extern crate env_logger;
+
+use test::Bencher;
+use std::sync::Once;
+
+fn init_once() {
+    static ONCE: Once = Once::new();
+    ONCE.call_once(|| env_logger::init());
+}
+
+#[bench]
+fn log(b: &mut Bencher) {
+    init_once();
+
+    b.iter(|| {
+        debug!("fgfg");
+    })
+}
+
+#[bench]
+fn log_ndc_empty(b: &mut Bencher) {
+    init_once();
+
+    b.iter(|| {
+        ndc_debug!("fgfg");
+    })
+}
+
+#[bench]
+fn log_ndc_not_empty(b: &mut Bencher) {
+    init_once();
+
+    log_ndc::set_static_str("server");
+    b.iter(|| {
+        ndc_debug!("fgfg");
+    })
+}
