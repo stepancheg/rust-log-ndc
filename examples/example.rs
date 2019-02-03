a48ef24219d6993a42bf45b50dd79c70d0a0e037
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-03 23:14:09 +0000

    More utilities

--- examples/example.rs
+++ examples/example.rs
@@ -12,12 +12,12 @@ fn main() {
     ndc_info!("app started");
 
     thread::spawn(|| {
-        log_ndc::set_static_str("server");
+        log_ndc::set("server");
         ndc_info!("started");
     }).join().unwrap();
 
     thread::spawn(|| {
-        log_ndc::set_static_str("client");
+        log_ndc::set(format!("client {}", 1));
         ndc_error!("ah oh");
     }).join().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,23 @@ extern crate log;
 use std::rc::Rc;
 use std::sync::Arc;
 use std::cell::RefCell;
+use std::mem;
 
 mod macros;
 
 /// Diagnostic context
-enum Ndc {
+#[derive(Debug, Clone)]
+pub enum Ndc {
     Str(&'static str),
     String(String),
     Rc(Rc<String>),
     Arc(Arc<String>),
+}
+
+impl Default for Ndc {
+    fn default() -> Self {
+        Ndc::Str("")
+    }
 }
 
 impl Ndc {
@@ -27,37 +35,71 @@ impl Ndc {
     }
 }
 
+impl From<&'static str> for Ndc {
+    fn from(s: &'static str) -> Self {
+        Ndc::Str(s)
+    }
+}
+
+impl From<String> for Ndc {
+    fn from(s: String) -> Self {
+        Ndc::String(s)
+    }
+}
+
+impl From<Rc<String>> for Ndc {
+    fn from(s: Rc<String>) -> Self {
+        Ndc::Rc(s)
+    }
+}
+
+impl From<Arc<String>> for Ndc {
+    fn from(s: Arc<String>) -> Self {
+        Ndc::Arc(s)
+    }
+}
+
 thread_local! {
     static NDC: RefCell<Ndc> = RefCell::new(Ndc::Str(""));
 }
 
-fn set_ndc(ndc: Ndc) {
-    NDC.with(|r| *r.borrow_mut() = ndc);
+#[must_use]
+pub struct SetGuard {
+    ndc: Ndc,
 }
 
-/// Set thread-local context to a string.
-pub fn set_string(ndc: String) {
-    set_ndc(Ndc::String(ndc));
+impl Drop for SetGuard {
+    fn drop(&mut self) {
+        set(mem::replace(&mut self.ndc, Default::default()));
+    }
 }
 
-/// Set thread-local context to a `&'static str`.
-pub fn set_static_str(ndc: &'static str) {
-    set_ndc(Ndc::Str(ndc))
+/// Set thread-local context to a given string.
+pub fn set<S: Into<Ndc>>(ndc: S) {
+    replace(ndc);
 }
 
-/// Set thread-local context to `Rc<String>`
-pub fn set_rc_string(ndc: Rc<String>) {
-    set_ndc(Ndc::Rc(ndc))
-}
-
-/// Set thread-local context to `Arc<String>`
-pub fn set_arc_string(ndc: Arc<String>) {
-    set_ndc(Ndc::Arc(ndc))
+/// Set thread-local context to a given string and return previously stored value.
+pub fn replace<S: Into<Ndc>>(ndc: S) -> Ndc {
+    NDC.with(|r| {
+        mem::replace(&mut *r.borrow_mut(), ndc.into())
+    })
 }
 
 /// Clear diagnostic context
 pub fn clear() {
-    set_static_str("");
+    set("");
+}
+
+/// Set thread-local context and return a guard which sets previous value on drop.
+pub fn push<S: Into<Ndc>>(ndc: S) -> SetGuard {
+    SetGuard { ndc: replace(ndc) }
+}
+
+/// Get a copy of the raw NDC object.
+/// Return NDC with empty string if unset.
+pub fn get_raw() -> Ndc {
+    NDC.with(|r| r.borrow().clone())
 }
 
 /// Get a copy of the string in thread-local context.
@@ -69,4 +111,33 @@ pub fn get_copy() -> String {
 /// Read a thread-local context with provided callback.
 pub fn get<R, F>(f: F) -> R where F: FnOnce(&str) -> R {
     NDC.with(|r| f(r.borrow().to_str()))
+}
+
+#[cfg(test)]
+mod test {
+    mod log_ndc {
+        pub use super::super::*;
+    }
+
+    #[test]
+    fn set_get() {
+        let _g = log_ndc::push("");
+
+        log_ndc::set("aa");
+        assert_eq!("aa", log_ndc::get_copy());
+    }
+
+    #[test]
+    fn push() {
+        let _g = log_ndc::push("");
+
+        let _g1 = log_ndc::push("aa");
+        assert_eq!("aa", log_ndc::get_copy());
+
+        let g2 = log_ndc::push("bb");
+        assert_eq!("bb", log_ndc::get_copy());
+        drop(g2);
+
+        assert_eq!("aa", log_ndc::get_copy());
+    }
 }
