cb45bd24d0c033623326dc61e2dfda2d983e9d16
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-03 23:59:35 +0000

    Better example

--- examples/example.rs
+++ examples/example.rs
@@ -3,12 +3,15 @@ extern crate log_ndc;
 
 use std::thread;
 
-
+// Try it with command: `cargo run --example example`
 fn main() {
+    // Initialize logger
+    // (could be as simple as `env_logger::init`)
     let mut builder = env_logger::Builder::new();
     builder.parse("debug");
     builder.init();
 
+    // There's not NDC here, this call is equivalent to `info!(...)` call.
     ndc_info!("app started");
 
     thread::spawn(|| {
