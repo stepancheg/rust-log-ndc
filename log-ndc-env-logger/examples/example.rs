cd802096fab56f241c909c362ba77a46befd543b
Stepan Koltsov <stepan.koltsov@gmail.com>
2019-02-07 01:08:53 +0000

    Better example

--- log-ndc-env-logger/examples/example.rs
+++ log-ndc-env-logger/examples/example.rs
@@ -6,20 +6,23 @@ use std::thread;
 // Try it with command: `RUST_LOG=debug cargo run --example example`
 fn main() {
     // Initialize logger
-    // (could be as simple as `ndc_log_env_logger::init()` for default settings)
     log_ndc_env_logger::init();
 
-    // No NDC here
+    // Outputs:
+    // INFO 2019-02-07T01:08:07Z: example: app started
     info!("app started");
 
     thread::spawn(|| {
         log_ndc::set("server");
-        // Should print `[server] started`
+        // Outputs:
+        // INFO 2019-02-07T01:08:07Z: example: [server] started
         info!("started");
     }).join().unwrap();
 
     thread::spawn(|| {
         log_ndc::set(format!("client {}", 17));
+        // Outputs:
+        // ERROR 2019-02-07T01:08:07Z: example: [client 17] ah oh
         error!("ah oh");
     }).join().unwrap();
 }
